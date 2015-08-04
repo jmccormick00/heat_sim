369832fe3ce1f94bbdd7df60ce8fdf23b62b29a8
jmccormick00 <jmccormick00@gmail.com>
2015-08-03 23:02:37 -0400

    Added the simulation code

--- heat_sim.py
+++ heat_sim.py
@@ -1,6 +1,6 @@
 __author__ = 'jr'
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def check(res):
     tol=10e-4
@@ -27,7 +27,7 @@ d2 = 0.0001  # m
 
 #--------------------------------------------------------------------------
 # Thermal Properties
-k1 = 236  # W/mK
+k = 236  # W/mK
 q = 61000  # W/m^2
 h = 7.9  # W/Km^2
 Te = 300  # K Temperature of the ambient air
@@ -37,47 +37,67 @@ density = 2702  # kg/m^3
 c = 903  # j/(kgK)
 
 # Compute constants
+# These are pulled out of the grid equations to save a few multiplications
+# each iteration
 U = 1/(d2/k2 + 1/h)
-Xi = 2*k1*d1 + U*delta*delta
-Yi = 3*k1*d1 + U*delta*delta
+Xi = 2*k*d1 + U*delta*delta
+Yi = 3*k*d1 + U*delta*delta
+Zi = 4*k*d1 + U*delta*delta
 D = Te*U*delta*delta
 
 #--------------------------------------------------------------------------
-ROW = panHeight/delta + 1
-COLUMN = panWidth/delta + 1
+ROW = int(panHeight/delta + 1)
+COLUMN = int(panWidth/delta + 1)
 
-# Solve the for where the Eye is in the Grid
+# Solve for where the Eye is in the Grid
 c1 = panHeight/2 - eyeHeight/2
 c2 = panWidth/2 - eyeWidth/2
-Ieye = c1/delta + 0.5
-Jeye = c2/delta + 0.5
+Ieye = c1/delta
+Jeye = c2/delta
 Ieye = int(Ieye)
 Jeye = int(Jeye)
 Eye = [Ieye, Jeye, Ieye + eyeHeight/delta, Jeye + eyeWidth/delta]
 #--------------------------------------------------------------------------
 
 # Set the grid to a initial temp 450 K
-grid = np.empty((10, 10))
+grid = np.empty((ROW, COLUMN))
 grid.fill(450.0)
 
 while True:
     oldGrid = grid.copy()
-    # Calculate the corners
+    for i in xrange(ROW):
+        for j in xrange(COLUMN):
+            # Calculate the corners
+            if i == 0 & j == 0:  # Top left
+                grid[i, j] = (k*d1*(grid[i, j+1] + grid[i+1, j]) + D) / Xi
+            elif i == 0 & j == (COLUMN-1):  # Top right
+                grid[i, j] = (k*d1*(grid[i, j-1] + grid[i+1, j]) + D) / Xi
+            elif i == (ROW-1) & j == 0:  # Bottom left
+                grid[i, j] = (k*d1*(grid[i-1, j] + grid[i, j+1]) + D) / Xi
+            elif i == (ROW-1) & j == (COLUMN-1):  # Bottom Right
+                grid[i, j] = (k*d1*(grid[i-1, j] + grid[i, j-1]) + D) / Xi
 
-    # Calculate the sides
+            # Calculate the sides
+            elif i == 0 & j > 0 & j < (COLUMN-1):  # Top
+                grid[i, j] = (k*d1*(grid[i, j+1] + grid[i+1, j] + grid[i, j-1]) + D) / Yi
+            elif i == (ROW-1) & j > 0 & j < (COLUMN-1):  # Bottom
+                grid[i, j] = (k*d1*(grid[i, j+1] + grid[i-1, j] + grid[i, j-1]) + D) / Yi
+            elif j == 0 & i > 0 & i < (ROW-1):  # Left
+                grid[i, j] = (k*d1*(grid[i-1, j] + grid[i, j+1] + grid[i+1, j]) + D) / Yi
+            elif j == (COLUMN-1) & i > 0 & i < (ROW-1):  # Right
+                grid[i, j] = (k*d1*(grid[i-1, j] + grid[i, j-1] + grid[i+1, j]) + D) / Yi
 
-    # Over the eye
+            # Over the eye
+            elif i >= Eye[0] & i <= Eye[2] & j >= Eye[1] & j <= Eye[3]:
+                grid[i, j] = (k*d1*(grid[i-1, j] + grid[i, j-1] + grid[i+1, j] + grid[i, j+1]) + q*delta*delta + D) / Zi
 
-    # outside the eye
+            # outside the eye
+            else:
+                grid[i, j] = (k*d1*(grid[i-1, j] + grid[i, j-1] + grid[i+1, j] + grid[i, j+1]) + D) / Zi
 
     if check(np.abs(grid-oldGrid)):
         break
 
-
-
-
-
-
-
-
-
+# plot the contour plot of the grid
+CS = plt.contour()
+plt.show()
